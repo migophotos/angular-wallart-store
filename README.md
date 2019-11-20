5c8e6f4645305af320ba7f8709377f1a440219c7
migophotos <guiappdev@gmail.com>
2019-11-20 17:41:58 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-wc2fj2
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-wc2fj2)
---
+++ src/app/products.ts
@@ -0,0 +1,29 @@
+export const products = [
+  {
+    name: 'Лето на Волге',
+    price: 100,
+    description: 'Фотография из путешествия по великой русской реке Волге'
+  },
+  {
+    name: 'Осенний лес',
+    price: 150,
+    description: 'Желтые листья в лесу'
+  },
+  {
+    name: 'Красные цветы Израиля',
+    price: 120,
+    description: 'Цветущие каланиет и маки весной в Израиле'
+  },
+  {
+    name: 'Желтые цветы Израиля',
+    price: 920,
+    description: 'Цветущие желтые цветы, весной в Израиле'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
